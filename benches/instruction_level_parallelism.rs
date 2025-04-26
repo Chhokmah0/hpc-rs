f02770d136a8a236f627fcb7550b00c7944c639e
Chhokmah0 <haoyimin1214@foxmail.com>
2025-04-26 15:09:01 +0800

    refactor: 修改 benchmark 逻辑

--- benches/instruction_level_parallelism.rs
+++ benches/instruction_level_parallelism.rs
@@ -3,23 +3,14 @@ use hpc_rs::instruction_level_parallelism::{branchless_programming::*, the_cost_
 
 fn bench_branch_cost(c: &mut Criterion) {
     c.bench_function("branch cost", |b| b.iter(|| branch_cost()));
-}
-
-fn bench_branch_cost_sort(c: &mut Criterion) {
     c.bench_function("branch cost when sort", |b| b.iter(|| branch_cost_sort()));
-}
-
-fn bench_branch_cost_likely(c: &mut Criterion) {
     c.bench_function("branch cost when likely", |b| b.iter(|| branch_cost_likely()));
 }
 
 fn bench_branchless_cost(c: &mut Criterion) {
     c.bench_function("branchless cost", |b| b.iter(|| branchless_cost()));
-}
-
-fn bench_branchless_no_volatile(c: &mut Criterion) {
     c.bench_function("branchless no volatile", |b| b.iter(|| branchless_no_volatile()));
 }
 
-criterion_group!(benches, bench_branch_cost, bench_branchless_cost, bench_branchless_no_volatile);
+criterion_group!(benches, bench_branch_cost, bench_branchless_cost);
 criterion_main!(benches);
